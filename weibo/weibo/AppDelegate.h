cd39e31abbba8d6cee9111b020b2fb6cd84a75d2
renhaopingzhi <renhao123234@126.com>
2016-10-08 14:42:16 +0800

    创建了一个 项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by 于亚伟 on 2016/10/8.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
